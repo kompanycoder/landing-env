ebec77e62c3d0e06492e6af53b73d38e012c2b94
kompanycoder <mwegagideon@gmail.com>
2019-10-02 08:59:48 +0300

    trying out the sendfile method from express

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+// const path = require('path');
 const port = process.env.PORT || 8080;
 const App = express();
-// App.use(express.static(__dirname + ''));
+// App.use(express.static(__dirname + '/public'));
 App.use(bodyParser.json());
+App.use(cors());
 App.get('/', (req, res)=>{
     // res.render();
-    res.json({"message":"Hello World"});
+    res.sendFile('/public/index.html', (err, results)=>{
+        if(err) return err;
+        results
+    });
 });
 
 
