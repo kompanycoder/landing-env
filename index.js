e7bcab4f5b26c7a7bc01654ee46e721de06679a4
kompanycoder <mwegagideon@gmail.com>
2019-10-02 09:10:13 +0300

    dashboard route connected sussessfully

--- index.js
+++ index.js
@@ -9,15 +9,16 @@ App.use(bodyParser.json());
 App.use(bodyParser.urlencoded({extended: true, }));
 App.use(cors());
 App.get('/', (req, res)=>{
-    // res.render();
-    res.sendFile('/public/index.html', (err, results)=>{
-        if(err) return err;
-        results
-    });
+    res.send("Hello from developer dashboard app");
+    // res.render()
+    // res.sendFile('/public/index.html', (err, results)=>{
+    //     if(err) return err;
+    //     results
+    // });
 });
 
 
-App.listen(()=>{
+App.listen(port,()=>{
     console.log("Dev env initialised on :"+ port);
 })
 
