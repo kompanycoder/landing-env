c6988fa411b52e30ba53878bca461d8371dfb7ab
kompanycoder <mwegagideon@gmail.com>
2019-10-02 09:07:40 +0300

    fixed url encoded warning

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 8080;
 const App = express();
 // App.use(express.static(__dirname + '/public'));
 App.use(bodyParser.json());
+App.use(bodyParser.urlencoded({extended: true, }));
 App.use(cors());
 App.get('/', (req, res)=>{
     // res.render();
