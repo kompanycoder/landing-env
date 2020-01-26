9094875785cb5c5c8377272defe2ea8f29687494
kompanycoder <mwegagideon@gmail.com>
2020-01-26 19:41:12 +0300

    reinitialised client-side due to unsolvable err on sass-loader not working afresh

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,15 +1,8 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeEnvComponent } from './home-env/home-env.component';
-import { GenesisComponent } from './genesis/genesis.component';
-import { PlaygroundComponent } from './playground/playground.component'
 
-const routes: Routes = [
-  {path: 'pandora', component: HomeEnvComponent},
-  {path: 'genesis', component: GenesisComponent},
-  {path: 'playground', component: PlaygroundComponent},
-  {path: '', redirectTo: 'pandora' , pathMatch: 'full'}
-];
+
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,27 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeEnvComponent } from './home-env/home-env.component';
-import { GenesisComponent } from './genesis/genesis.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { PlaygroundComponent } from './playground/playground.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeEnvComponent,
-    GenesisComponent,
-    HeaderComponent,
-    FooterComponent,
-    PlaygroundComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: []
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
